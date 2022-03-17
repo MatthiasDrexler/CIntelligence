e88a31932259192f63dadeab881ea462e592cacb
Matthias Drexler <matthias.drexler@andrena.de>
2022-03-18 00:00:55 +0100

    Activate extension when yaml file is opened

--- src/test/runTest.ts
+++ src/test/runTest.ts
@@ -4,16 +4,16 @@ import { runTests } from '@vscode/test-electron';
 
 async function main() {
 	try {
-		// The folder containing the Extension Manifest package.json
-		// Passed to `--extensionDevelopmentPath`
 		const extensionDevelopmentPath = path.resolve(__dirname, '../../');
-
-		// The path to test runner
-		// Passed to --extensionTestsPath
 		const extensionTestsPath = path.resolve(__dirname, './suite/index');
 
+        const version = 'insiders';
+
+        const testWorkspace = path.resolve(extensionDevelopmentPath, 'src', 'test');
+        const launchArgs = ["--new-window", testWorkspace];
+
 		// Download VS Code, unzip it and run the integration test
-		await runTests({ extensionDevelopmentPath, extensionTestsPath });
+		await runTests({ extensionDevelopmentPath, extensionTestsPath, version, launchArgs });
 	} catch (err) {
 		console.error('Failed to run tests');
 		process.exit(1);
--- src/test/suite/extension.test.ts
+++ src/test/suite/extension.test.ts
@@ -1,15 +1,21 @@
-import * as assert from 'assert';
-
-// You can import and use all API from the 'vscode' module
-// as well as import your extension to test it
+import { expect } from 'chai';
+import * as path from 'path';
 import * as vscode from 'vscode';
-// import * as myExtension from '../../extension';
 
-suite('Extension Test Suite', () => {
-	vscode.window.showInformationMessage('Start all tests.');
+suite('CIntelligence', () => {
+    const CINTELLIGENCE_EXTENSION_ID = "cintelligence.cintelligence";
+    const YAML_FILE_PATH = path.join("resources", "yaml", "empty.yml");
+    const MAX_LOADING_THRESHOLD_IN_MILLISECONDS = 100;
 
-	test('Sample test', () => {
-		assert.strictEqual(-1, [1, 2, 3].indexOf(5));
-		assert.strictEqual(-1, [1, 2, 3].indexOf(0));
-	});
+    test('should be activated if yaml file is opened', async () => {
+        const testWorkspace = vscode.workspace.workspaceFolders![0];
+        const pathToYamlFile = path.join(testWorkspace.uri.fsPath, YAML_FILE_PATH);
+        const openedYamlFile = await vscode.workspace.openTextDocument(vscode.Uri.file(pathToYamlFile));
+        await vscode.window.showTextDocument(openedYamlFile);
+        await new Promise(it => setTimeout(it, MAX_LOADING_THRESHOLD_IN_MILLISECONDS));
+
+        const cintelligence = vscode.extensions.getExtension(CINTELLIGENCE_EXTENSION_ID);
+
+        expect(cintelligence?.isActive).to.be.true;
+    });
 });
