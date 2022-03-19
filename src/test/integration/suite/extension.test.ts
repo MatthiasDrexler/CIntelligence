bae8d4764efb91a26a2dbba62922f29f9bb36838
Matthias Drexler <matthias.drexler@andrena.de>
2022-03-19 12:07:28 +0100

    Use bdd interface

--- src/test/integration/suite/extension.test.ts
+++ src/test/integration/suite/extension.test.ts
@@ -2,12 +2,12 @@ import { expect } from 'chai';
 import * as path from 'path';
 import * as vscode from 'vscode';
 
-suite('CIntelligence', () => {
+describe('CIntelligence', () => {
     const CINTELLIGENCE_EXTENSION_ID = "cintelligence.cintelligence";
     const YAML_FILE_PATH = path.join("resources", "yaml", "empty.yml");
     const MAX_LOADING_THRESHOLD_IN_MILLISECONDS = 100;
 
-    test('should be activated if yaml file is opened', async () => {
+    it('should be activated if yaml file is opened', async () => {
         const testWorkspace = vscode.workspace.workspaceFolders![0];
         const pathToYamlFile = path.join(testWorkspace.uri.fsPath, YAML_FILE_PATH);
         const openedYamlFile = await vscode.workspace.openTextDocument(vscode.Uri.file(pathToYamlFile));
