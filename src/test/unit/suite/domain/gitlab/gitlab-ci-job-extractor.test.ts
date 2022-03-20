47da5426e7c8d5bc80bcf25535df90c6cecbf149
Matthias Drexler <matthias.drexler@andrena.de>
2022-03-20 19:49:07 +0100

    Add test for files containing just a scalar

--- src/test/unit/suite/domain/gitlab/gitlab-ci-job-extractor.test.ts
+++ src/test/unit/suite/domain/gitlab/gitlab-ci-job-extractor.test.ts
@@ -14,6 +14,15 @@ describe("GitlabCiJobExtractor", () => {
         expect(extractedJobs).to.have.length(0);
     })
 
+    it("should return empty list for scalar yaml file", () => {
+        const resourceFile = path.join("yaml", "scalar.yml");
+        const yamlDocument = new ResourceParser().parse(resourceFile);
+
+        const extractedJobs = new GitlabCiJobExtractor().extract([yamlDocument]);
+
+        expect(extractedJobs).to.have.length(0);
+    })
+
     it("should return job-1 and job-2 for gitlab-ci.twoJobs.yml", () => {
         const resourceFile = path.join("yaml", "gitlab", "gitlab-ci.twoJobs.yml");
         const yamlDocument = new ResourceParser().parse(resourceFile);
