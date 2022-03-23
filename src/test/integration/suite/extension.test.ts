bfe05ce97b06f4c93d08c0356bf2e6525140176e
Matthias Drexler <matthias.drexler@andrena.de>
2022-03-23 22:48:59 +0100

    Avoid extension name in source code

--- src/test/integration/suite/extension.test.ts
+++ src/test/integration/suite/extension.test.ts
@@ -1,15 +1,15 @@
 import { expect } from 'chai';
 import * as vscode from 'vscode';
 
-describe('CIntelligence', function () {
-    const CINTELLIGENCE_EXTENSION_ID = "cintelligence.cintelligence";
+describe('Extension', function () {
+    const EXTENSION_ID = "cintelligence.cintelligence";
     const MAX_LOADING_THRESHOLD_IN_MILLISECONDS = 100;
 
     it('should be activated if workspace with .gitlab-ci.yaml file is opened', async function () {
         await new Promise(it => setTimeout(it, MAX_LOADING_THRESHOLD_IN_MILLISECONDS));
 
-        const cintelligence = vscode.extensions.getExtension(CINTELLIGENCE_EXTENSION_ID);
+        const extension = vscode.extensions.getExtension(EXTENSION_ID);
 
-        expect(cintelligence?.isActive).to.be.true;
+        expect(extension?.isActive).to.be.true;
     });
 });
